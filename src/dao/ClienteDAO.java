ebc75b4982aca91aafdcceaa4acc285fcd4a0aa5
Emanoely Araújo <85528024+emanoelyaraujo@users.noreply.github.com>
2021-09-22 21:46:36 -0300

    método remove ClienteDAO

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -57,46 +57,132 @@ public class ClienteDAO {
         // retorna a coleção
         return clientes;
     }
-    
-    public String grave(Cliente cliente){
+
+    public String grave(Cliente cliente) {
         // declara a string que será retornada
         String msg = "";
-        
+
         try {
-            
+
             // efetua uma conexão com o BD
             Conexao conn = new Conexao();
             conn.conecte();
-            
+
             // determina a query que será executada 
             String sql = "call p_salve_cliente(?, ?, ?, ?, ?, ?)";
-            
+
             // cria um objeto que substirui a ? 
             PreparedStatement st = conn.conexao.prepareStatement(sql);
-            
+
             st.setInt(1, cliente.getPessoaId());
             st.setString(2, cliente.getNome());
             st.setString(3, cliente.getTipoPessoa());
             st.setString(4, cliente.getCpfCnpj());
             st.setString(5, cliente.getTelefone());
             st.setString(6, cliente.getEmail());
-            
+
             // executa a procedure
             st.executeUpdate();
-            
+
             // recupera a msg do BD
             ResultSet rs = st.getResultSet();
-            rs.next(); 
-            
+            rs.next();
+
             // atribui a variavel msg a mensagem da procedure
             msg = rs.getString(3);
             st.close();
-            
+
         } catch (ClassNotFoundException | SQLException e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
-        
+
         // retorna a msg da procedure
         return msg;
+    }
+
+    public String remove(int pessoaId) {
+
+        // string que irá retornar a msg da procedure
+        String msg = "";
+
+        try {
+
+            // efetua uma conexao com o banco
+            Conexao conn = new Conexao();
+            conn.conecte();
+
+            // determina a query a ser executada no BD
+            String sql = "call p_delete_cliente (?)";
+
+            // substitui o coringa ?
+            PreparedStatement st = conn.conexao.prepareStatement(sql);
+
+            st.setInt(1, pessoaId);
+
+            // executa a procedure
+            st.executeUpdate();
+
+            // recupera a msg da procedure
+            ResultSet result = st.getResultSet();
+            result.next();
+
+            msg = result.getString(3);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+        return msg;
+    }
+
+    public ArrayList<Cliente> pesquise(String nome) {
+
+        // declara a coleção de objetos 
+        ArrayList<Cliente> clientes = new ArrayList<>();
+
+        try {
+
+            // efetua uma conexao com o bd
+            Conexao conn = new Conexao();
+            conn.conecte();
+
+            // determina a query de busca
+            String sql = "select * from v_cliente where nome like ? order by nome";
+
+            // substitui o coringa ? 
+            PreparedStatement st = conn.conexao.prepareStatement(sql);
+
+            // substitui a ? pelo argumento nome
+            st.setString(1, "%" + nome + "%");
+
+            // executa a query
+            st.executeQuery();
+            ResultSet rs = st.getResultSet();
+
+            // cria um obj para formatar data
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+            while (rs.next()) {
+
+                // cria um objeto da classe cliente
+                Cliente cliente = new Cliente(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        format.parse(rs.getString(7))
+                );
+
+                // adiciona o objeto da classe cliente
+                clientes.add(cliente);
+            }
+
+        } catch (ClassNotFoundException | SQLException | ParseException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+        return clientes;
     }
 }
